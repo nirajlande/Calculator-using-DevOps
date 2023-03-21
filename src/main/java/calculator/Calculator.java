fb10c020ed69ede9db34416656ac7afb9d3a384f
Niraj <nirajlande419@gmail.com>
2023-03-22 00:34:35 +0530

    g

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,14 @@
 package calculator;
 import java.util.InputMismatchException;
-import java.util.*;
+import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
     public static void main(String[] args){
+
+        Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
         int choices, flag=0;
         double num, exp;
